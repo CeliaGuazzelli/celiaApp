de70587bdb02db7266bf59ec224d66cdb3cb95b5
Celia Guazzelli <celiaguazzelli@gmail.com>
2021-06-11 16:26:17 -0300

    Adicionei array de séries e a interface de séries

---
+++ src/app/models/ISerie.model.ts
@@ -0,0 +1,8 @@
+export interface ISerie {
+  snome: string;
+  slancamento: string;
+  sduracao: string;
+  sclass: number;
+  scartaz: string;
+  sgeneros: string[];
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,3 +1,4 @@
+import { ISerie } from './../models/ISerie.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
@@ -9,6 +10,59 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+
+titulo = 'Séries';
+
+listaSeries: ISerie[] = [
+  {
+    snome: 'Game of Trones',
+    slancamento: '2011',
+    sduracao: '1h',
+    sclass: 86,
+    scartaz: 'https://www.themoviedb.org/t/p/w220_and_h330_face/mQ9cyw1gfpK1M3a69cgXFHvWkih.jpg',
+    sgeneros: ['Fantasy', 'Drama', 'Adventure']
+  },
+  {
+    snome: 'Friends',
+    slancamento: '1994',
+    sduracao: '25m',
+    sclass: 84,
+    scartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/f496cm9enuEsZkSPzCwnTESEK5s.jpg',
+    sgeneros: ['Comedy', 'Drama']
+  },
+  {
+    snome: 'O mentalista',
+    slancamento: '2008',
+    sduracao: '43m',
+    sclass: 87,
+    scartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/d1ZcT5tHzUeQ7zgYecOVoWxH9FL.jpg',
+    sgeneros: ['Crime', 'Drama', 'Mistery']
+  },
+  {
+    snome: 'Sou Luna',
+    slancamento: '2016',
+    sduracao: '45m',
+    sclass: 80,
+    scartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/9mENBXLVIvGimTnU5rHL39q9ETR.jpg',
+    sgeneros: ['Kids', 'Soup']
+  },
+  {
+    snome: 'Lista Negra',
+    slancamento: '2013',
+    sduracao: '43m',
+    sclass: 74,
+    scartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ziy2LUOZOA3Ob5hGZAvKXCmvwe9.jpg',
+    sgeneros: ['Crime', 'Drama', 'Mistery']
+  },
+  {
+    snome: 'The Flash',
+    slancamento: '2014',
+    sduracao: '44m',
+    sclass: 77,
+    scartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg',
+    sgeneros: ['Drama', 'Fantasy']
+  }
+];
 
   constructor(public alertController: AlertController, public toastController: ToastController) {}
 
